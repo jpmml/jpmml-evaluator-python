856adf0877aa209fb1d191fe48f24ee91f25113d
Villu Ruusmann <villu.ruusmann@gmail.com>
2023-03-02 11:00:48 +0200

    Refactored unit tests

--- jpmml_evaluator/tests/test_backend.py
+++
@@ -1,6 +0,0 @@
-# Both JPype and PyJNIus tests would like to create a new JVM instance.
-# Unfortunately, a Python process can only be associated with one JVM instance, 
-# so it will be necessary to disable one of the two backends.
-from .jpype import JPypeEvaluatorTest
-from .py4j import Py4JEvaluatorTest
-#from .pyjnius import PyJNIusEvaluatorTest
